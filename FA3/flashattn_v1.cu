f7c6b865ffa5cd168dbb9e22b2a43a0756d657db
sampan26 <114320314+sampan26@users.noreply.github.com>
2025-10-11 22:04:19 -0700

    incomplete mma

--- FA3/flashattn_v1.cu
+++ FA3/flashattn_v1.cu
@@ -24,6 +24,39 @@
 
 using namespace cute;
 
+template<bool init=false, int wg_wait=0, typename TensorA, typename TensorB, typename TensorC, typename TiledMma>
+__forceinline__ __device__ void gemm(TiledMma& mma, TensorA const& tCrA, TensorB const& tCrB, TensorC const& tCrC) {
+    constexpr bool Is_RS = 
+        !cute::is_base_of<cute::GMMA:DescriptorIterator, typename TiledMma::FrgTypeA>::value;
+    if constexpr(Is_RS) {
+        warpgroup_fence_operand(const_cast<TensorA&>(tCrA));
+    }
+    warpgroup_fence_operand(tCrC);
+    warpgroup_arrive();
+    if constexpr (init) {
+        mma.accumulate_ = GMMA::ScaleOut::Zero;
+        CUTLASS_PRAGMA_UNROLL
+        for (int k_block = 0; k_block < size<2>(tCrA); ++k_block) {
+            cute::gemm(mma, tCrA(_,_,k_block), tCrB(_,_,k_block), tCrC);
+            mma.accumulate_ = GMMA::ScaleOut::One;
+        }
+    } else {
+        CUTLASS_PRAGMA_UNROLL
+        for (int k_block = 0; k_block < size<2>(tCrA); ++k_block) {
+            cute::gemm(mma, tCrA(_,_,k_block), tCrB(_,_,k_block), tCrC);
+            mma.accumulate_ = GMMA::ScaleOut::One;
+        }
+    }
+    warpgroup_commit_batch();
+    if constexpr(wg_wait >= 0) {
+        warpgroup_wait<wg_wait>();
+    }
+    warpgroup_fence_operand(tCrC);
+    if constexpr (Is_RS) {
+        warpgroup_fence_operand(const_cast<TensorA&>(tCrA));
+    }
+}
+
 template <int STAGES, class TQ, class TK, class TV, class TO, 
          class SmemLayoutQ, class SmemLayoutK, class SmemLayoutV, class SmemLayoutO>
 struct SharedStorage {
@@ -44,12 +77,13 @@ struct SharedStorage {
 // Device kernel template
 template <typename TQ, typename TK, typename TV, typename TO, 
             class CtaTiler,
+            class LayoutQ, class LayoutK, class LayoutV,
             class SmemLayoutQ, class TmaQ,
             class SmemLayoutK, class TmaK,
             class SmemLayoutV, class TmaV,
-            class SmemLayoutO, 
+            class SmemLayoutVt, class SmemLayoutO, 
             class TiledMmaQK, class TiledMmaPV,
-            int kWarps, int kWarpGroups, int kConsumerWGs, int kThreads,
+            int kWarps, int kWarpGroups, int kConsumerThreads, int kThreads,
             int bM, int bN, int bK, int PIPE, int cluster_M>
 __global__ static
 __launch_bounds__(kThreads, 1)
@@ -59,9 +93,15 @@ flash_attn_device(CtaTiler cta_tiler,
                     TQ const* Q, CUTLASS_GRID_CONSTANT TmaQ const tma_q,
                     TK const* K, CUTLASS_GRID_CONSTANT TmaK const tma_k,
                     TV const* V, CUTLASS_GRID_CONSTANT TmaV const tma_v,
-                    TO* O,
-                    TiledMmaQK mma_qk, TiledMmaPV mma_pv)
+                    TO* O, 
+                    int bs, int seq_len, int NUM_HEADS, int head_dim)
 {
+    const int q_tile_idx = blockIdx.x;
+    const int batch_head_idx = blockIdx.y;
+    const int batch_idx = batch_head_idx / NUM_HEADS;
+    const int head_idx = batch_head_idx % NUM_HEADS;
+    const int num_kv_tiles = cute::ceil_div((q_tile_idx + 1) * get<0>(cta_tiler), get<1>(cta_tiler));
+
     using MainloopPipeline = cutlass::PipelineTmaAsync<PIPE>;
     using PipelineState = typename MainloopPipeline::PipelineState;
     typename MainloopPipeline::Params params;
@@ -81,11 +121,143 @@ flash_attn_device(CtaTiler cta_tiler,
     }
 
     params.is_leader = threadIdx.x % 128 == 0;
-    params.num_consumers = kConsumerWGs * 128;
+    params.num_consumers = kConsumerThreads;
+    params.role = warp_group_idx == 0 ? MainloopPipeline::ThreadCategory::Producer : MainloopPipeline::ThreadCategory::Consumer;
 
     static constexpr uint32_t TmaTransactionBytesQ = static_cast<uint32_t>(size(SmemLayoutQ{}) * cutlass::sizeof_bits_v<TQ> / 8);
-    static constexpr uint32_t TmaTransactionBytesk = static_cast<uint32_t>(size(take<0,2>(SmemLayoutK{})) * cutlass::sizeof_bits_v<TQ> / 8); // faster than init a new aligned_array or array_engine
+    static constexpr uint32_t TmaTransactionBytesK = static_cast<uint32_t>(size(take<0,2>(SmemLayoutK{})) * cutlass::sizeof_bits_v<TQ> / 8); // faster than init a new aligned_array or array_engine
     static constexpr uint32_t TmaTransactionBytesV = static_cast<uint32_t>(size(take<0,2>(SmemLayoutV{})) * cutlass::sizeof_bits_v<TQ> / 8); 
+
+    params.transaction_bytes = TmaTransactionBytesK;
+
+    if (warp_idx == 0 && lane_predicate) {
+        smem.barrier_Q.init(1);
+        smem.barrier_O.init(1); //cluster size
+    }
+
+    MainloopPipeline pipeline_k(smem.pipeline_k, params, Shape<_1, _1, _1>{});
+    MainloopPipeline pipeline_v(smem.pipeline_v, params, Shape<_1, _1, _1>{});
+
+    __syncthreads();
+
+    if (warp_group_idx == 0) {
+        cutlass::arch::warpgroup_reg_dealloc<24>();
+
+        int warp_idx_in_wg = __shfl_sync(0xfffffff, (threadIdx.x / 32) % 4, 0);
+        if (warp_idx_in_wg == 0) {
+            PipelineState smem_pipe_write_k = cutlass::make_producer_start_state<MainloopPipeline>();
+            PipelineState smem_pipe_write_v = cutlass::make_producer_start_state<MainloopPipeline>();
+
+            Tensor sQ = make_tensor(make_smem_ptr(smem.smem_q.data()), SmemLayoutQ{});
+            Tensor sK = make_tensor(make_smem_ptr(smem.smem_k.data()), SmemLayoutK{});
+            Tensor sV = make_tensor(make_smem_ptr(smem.smem_v.data()), SmemLayoutV{});
+
+            Tensor mQ = tma_q.get_tma_tensor(LayoutQ{}.shape());
+            Tensor mK = tma_k.get_tma_tensor(LayoutK{}.shape());
+            Tensor mV = tma_v.get_tma_tensor(LayoutV{}.shape());
+
+            Tensor gQ = local_tile(mQ(_, _, head_idx, batch_idx), select<0, 2>(cta_tiler), make_coord(q_tile_idx, _0{}));
+            Tensor gK = local_tile(mK(_, _, head_idx, batch_idx), select<1, 2>(cta_tiler), make_coord(_, _0{}));
+            Tensor gV = local_tile(mV(_, _, head_idx, batch_idx), select<2, 2>(cta_tiler), make_coord(_, _0{}));
+
+            Tensor sQ_x = make_tensor(sQ.data(), make_layout(sQ.layout(), Layout<_1>{}));
+            Tensor gQ_x = make_tensor(gQ.data(), make_layout(gQ.layout(), Layout<_1>{}));
+
+            auto [tQgQ, tQsQ] = tma_partition(tma_q, _0{}, Layout<_1>{}, group_modes<0,2>(sQ_x), group_modes<0,2>(gQ_x));
+            auto [tKgK, tKsK] = tma_partition(tma_k, _0{}, Layout<_1>{}, group_modes<0,2>(sK), group_modes<0,2>(gK));
+            auto [tVgV, tVsV] = tma_partition(tma_v, _0{}, Layout<_1>{}, group_modes<0,2>(sV), group_modes<0,2>(gV));
+
+            int kv_tile_idx = num_kv_tiles - 1;
+
+            int lane_predicate = cute::elect_one_sync();
+            if (lane_predicate) {
+                pipeline_k.producer_acquire(smem_pipe_write_k);
+                copy(tma_k.with(*pipeline_k.producer_get_barrier(smem_pipe_write_k), 0), 
+                    tKgK(_, kv_tile_idx), tKsK(_, smem_pipe_write_k.index()));
+                ++smem_pipe_write_k;
+            }
+
+            cutlass::arch::NamedBarrier::sync(kConsumerThreads + cutlass::NumThreadsPerWarp, 1);
+
+            if (lane_predicate) {
+                smem.barrier_Q.arrive_and_expect_tx(TmaTransactionBytesQ);
+                copy(tma_q.with(reinterpret_cast<cutlass::arch::ClusterTransactionBarrier::ValueType&>(
+                    smem.barrier_Q), 0),
+                    tQgQ, tQsQ);
+            }
+
+            smem.barrier_O.wait(1);
+
+            if (lane_predicate) {
+                #pragma unroll2
+                for (; kv_tile_idx > 0; --kv_tile_idx) {
+                    pipeline_k.producer_acquire(smem_pipe_write_k);
+                    copy(tma_k.with(*pipeline_k.producer_get_barrier(smem_pipe_write_k), 0), 
+                         tKgK(_, kv_tile_idx-1), tKsK(_, smem_pipe_write_k.index()));
+                    ++smem_pipe_write_k;
+                    pipeline_v.producer_acquire(smem_pipe_write_v);
+                    copy(tma_v.with(*pipeline_v.producer_get_barrier(smem_pipe_write_v), 0), 
+                         tVgV(_, kv_tile_idx), tVsV(_, smem_pipe_write_v.index()));
+                    ++smem_pipe_write_v;
+                }
+            }
+            if (lane_predicate) {
+                pipeline_v.producer_acquire(smem_pipe_write_v);
+                    copy(tma_v.with(*pipeline_v.producer_get_barrier(smem_pipe_write_v), 0), 
+                         tVgV(_, kv_tile_idx), tVsV(_, smem_pipe_write_v.index()));
+                    ++smem_pipe_write_v;
+            }
+
+            if (warp_idx_in_wg == 0 && lane_predicate) {
+                pipeline_k.producer_tail(smem_pipe_write_k);
+                pipeline_v.producer_tail(smem_pipe_write_v);
+            }
+        }
+    } else {
+        cutlass::arch::warpgroup_reg_alloc<240>();
+        const int tid = threadIdx.x - cutlass::NumThreadsPerWarpGroup;
+        PipelineState smem_pipe_read_k, smem_pipe_read_v;
+
+        cutlass::arch::NamedBarrier::arrive(kConsumerThreads + cutlass::NumThreadsPerWarp, 1);
+        if (warp_group_idx > 1) {
+            cutlass::arch::NamedBarrier::arrive(kConsumerThreads, 3 + 1);
+        }
+        if constexpr (kConsumerThreads == 3 * cutlass::NumThreadsPerWarpGroup) {
+            if (warp_group_idx > 2) {
+                cutlass::arch::NamedBarrier::arrive(kConsumerThreads, 3 + 2);
+            }
+        }
+
+        TiledMmaQK tiled_mma_qk;
+        TiledMmaPV tiled_mma_pv;
+
+        Tensor sQ = make_tensor(make_smem_ptr(smem.smem_q.data()), SmemLayoutQ{});
+        Tensor sK = make_tensor(make_smem_ptr(smem.smem_k.data()), SmemLayoutK{});
+        Tensor sVt = make_tensor(make_smem_ptr(smem.smem_v.data()), SmemLayoutVt{});
+
+        auto thr_mma_qk = tiled_mma_qk.get_thread_slice(tid);
+        auto thr_mma_pv = tiled_mma_pv.get_thread_slice(tid);
+
+        Tensor tSrQ = thr_mma_qk.partition_fragment_A(sQ);
+        Tensor tSrK = thr_mma_qk.partition_fragment_B(sK);
+        Tensor tOrV = thr_mma_pv.partition_fragment_B(sVt);
+        Tensor tSrS = partition_fragment_C(tiled_mma_qk, select<0,1>(cta_tiler));
+        Tensor tOrO = partition_fragment_C(tiled_mma_pv, select<0,2>(cta_tiler));
+
+        tiled_mma_pv.accumulate_ = GMMA::ScaleOut::Zero;
+        int kv_tile_idx = num_kv_tiles - 1; 
+
+        cutlass::ConsumerToken token = static_cast<cutlass::BarrierStatus>(smem.barrier_Q.try_wait(0));
+        if (token == cutlass::BarrierStatus::WaitAgain) {
+            smem.barrier_Q.wait(0);
+        }
+
+        pipeline_k.consumer_wait(smem_pipe_read_k, pipeline_k.consumer_try_wait(smem_pipe_read_k));
+        cutlass::arch::NamedBarrier::sync(kConsumerThreads, 3 + warp_group_idx);
+        // gemm<true, -1>(tiled_mma_qk, tSrQ, tSrK(_,_,_,smem_pipe_read_k.index()), tSrS)
+        
+
+    }
 }
 
 template <class TQ, class TK, class TV, class TO, int D>
@@ -186,29 +358,27 @@ void run_flash_attn(int B, int T, int NH,
 
     using AtomLayoutMNK = Layout<Shape<Int<bM / 64>, _1, _1>>;    
 
-    TiledMMA TiledMmaQK = make_tiled_mma(GMMA::ss_op_selector<TQ, TK, float, TiledShape_MNK>(), AtomLayoutMNK{});
-    TiledMMA TiledMmaPV = make_tiled_mma(GMMA::ss_op_selector<TK, TV, float, TiledShape_MNK, GMMA::Major::K, GMMA::Major::MN>(), 
-                                        AtomLayoutMNK{});
+    using TiledMmaQK = decltype(make_tiled_mma(GMMA::ss_op_selector<TQ, TK, float, TiledShape_MNK>(), AtomLayoutMNK{}));
+    using TiledMmaPV = decltype(make_tiled_mma(GMMA::ss_op_selector<TK, TV, float, TiledShape_MNK, GMMA::Major::K, GMMA::Major::MN>(), 
+                                        AtomLayoutMNK{}));
     
     static constexpr int NUM_WARPS = ((bM / 64) + 1) * 4;
     static constexpr int NUM_THREADS = NUM_WARPS * 32;
     static constexpr int NUM_WARPGROUPS = NUM_WARPS / 4;
-    static constexpr int NUM_CONSUMER_GROUPS = NUM_WARPGROUPS - 1;
-    static constexpr int NUM_PRODUCER_GROUPS = 1;
-    static constexpr int NUM_CONSUMER_THREADS = NUM_CONSUMER_GROUPS * cutlass::NumThreadsPerWarp;
-    static constexpr int NUM_PRODUCER_THREADS = cutlass::NumThreadsPerWarp;
-    static constexpr int NUM_MMA_THREADS = size(TiledMmaQK);
+    static constexpr int NUM_CONSUMER_THREADS = size(TiledMmaQK{});
+    static constexpr int NUM_PRODUCER_THREADS = cutlass::NumThreadsPerWarp; 
 
     int num_tiles_q = cutlass::ceil_div(seq_len, bM);
     void const* kernel = reinterpret_cast<void const*>(
         &flash_attn_device<TQ, TK, TV, TO, 
                           decltype(TiledShape_MNK{}),
+                          decltype(LayoutQ), decltype(LayoutK), decltype(LayoutV),
                           decltype(SmemLayoutQ), decltype(tma_q),
                           decltype(SmemLayoutK), decltype(tma_k),
                           decltype(SmemLayoutV), decltype(tma_v),
-                          decltype(SmemLayoutO), 
-                          decltype(TiledMmaQK), decltype(TiledMmaPV),
-                          NUM_WARPS, NUM_WARPGROUPS, NUM_CONSUMER_GROUPS, NUM_THREADS,
+                          decltype(SmemLayoutVt), decltype(SmemLayoutO), 
+                          TiledMmaQK, TiledMmaPV,
+                          NUM_WARPS, NUM_WARPGROUPS, NUM_CONSUMER_THREADS,  NUM_THREADS,
                           bM, bN, HEAD_DIM, bP, CLUSER_M>);
 
     dim3 dimBlock(NUM_THREADS);
@@ -231,7 +401,7 @@ void run_flash_attn(int B, int T, int NH,
                                                             K, tma_k, 
                                                             V, tma_v,
                                                             O, 
-                                                            TiledMmaQK, TiledMmaPV);
+                                                            batch_size, seq_len, n_heads, HEAD_DIM);
 }
 
 template <class TQ, class TK, class TV, class TO>
