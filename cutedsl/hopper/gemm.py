1eb17a944ccad34f3842c81141b0f8ecd0cbd0ac
sampan26 <114320314+sampan26@users.noreply.github.com>
2025-10-18 01:55:42 -0700

    Update gemm.py

--- cutedsl/hopper/gemm.py
+++ cutedsl/hopper/gemm.py
@@ -22,7 +22,7 @@ class HopperGemm:
         self.consumer_warp_groups = math.prod(self.atom_layout_mnk)
         self.producer_warp_groups = 1
               
-
+    @cute.jit
     def __call__(
         self, 
         A: cute.Tensor, 
@@ -36,7 +36,7 @@ class HopperGemm:
 
 
         A_smemload_atom_struct = sm90_utils.get_smem_layout_atom(
-            cutlass.utils.layout.ROW_MAJOR,
+            cutlass.utils.layout.LayoutEnum.ROW_MAJOR,
             self.a_dtype,
             self.BK
         )
@@ -45,7 +45,7 @@ class HopperGemm:
             self.a_dtype
         )
         B_smemload_atom_struct = sm90_utils.get_smem_layout_atom(
-            cutlass.utils.layout.ROW_MAJOR,
+            cutlass.utils.layout.LayoutEnum.ROW_MAJOR,
             self.b_dtype,
             self.BK
         )
@@ -90,7 +90,7 @@ class HopperGemm:
             tiler_mn=(64, self.BN)
         )
 
-        tma_atom_a, tma_tensor_a = cute.nvgpu.cpasync.make_tma_tiled_atom(
+        tma_atom_a, tma_tensor_a = cute.nvgpu.cpasync.make_tiled_tma_atom(
             cute.nvgpu.cpasync.CopyBulkTensorTileGS2Op(),
             A,
             cute.slice_(sA_layout, (None, None, 0)),
@@ -98,7 +98,7 @@ class HopperGemm:
             num_multicast=1
         )
 
-        tma_atom_b, tma_tensor_b = cute.nvgpu.cpasync.make_tma_tiled_atom(
+        tma_atom_b, tma_tensor_b = cute.nvgpu.cpasync.make_tiled_tma_atom(
             cute.nvgpu.cpasync.CopyBulkTensorTileGS2Op(),
             B,
             cute.slice_(sB_layout, (None, None, 0)),
@@ -136,7 +136,7 @@ class HopperGemm:
         sA_layout: cute.ComposedLayout,
         sB_layout: cute.ComposedLayout,
     ):
-        bidx, _, _ = cute.arch.block_idx()
+        bidx, bidy, bidz = cute.arch.block_idx()
         tidx, _, _ = cute.arch.thread_idx()
         bdim, _, _ = cute.arch.block_dim()
 
@@ -144,7 +144,7 @@ class HopperGemm:
         storage = smem.allocate(self.shared_storage)
 
         sA = storage.sA.get_tensor(sA_layout.outer, swizzle=sA_layout.inner)
-        sB = storage.sB.get_tensor(sB_layout.outer, swizzle=sB_layout.outer)
+        sB = storage.sB.get_tensor(sB_layout.outer, swizzle=sB_layout.inner)
 
         warp_idx = cute.arch.make_warp_uniform(cute.arch.warp_idx())
         warp_idx_in_warp_group = cute.arch.make_warp_uniform(warp_idx % 4)
@@ -153,9 +153,57 @@ class HopperGemm:
         if warp_idx == 0:
             cute.nvgpu.cpasync.prefetch_descriptor(tma_atom_a)
             cute.nvgpu.cpasync.prefetch_descriptor(tma_atom_b)
+
+        producer_group = cutlass.utils.CooperativeGroup(cutlass.utils.Agent.Thread)
+        consumer_group = cutlass.utils.CooperativeGroup(cutlass.utils.Agent.Thread)
+
+        tma_load_bytes = (self.BM * self.BK + self.BN * self.BK) * 2
+
+        mainloop_pipeline = cutlass.utils.PipelineTmaAsync(
+            storage.ab_barrier_full.data_ptr(),
+            self.num_ab_pipeline_stages,
+            producer_group,
+            consumer_group,
+            tma_load_bytes,
+            cute.make_layout((1,1,1))
+        )
+
+
+        gA = cute.local_tile(mA, (self.BM, self.BK), (bidx, None))
+        gB = cute.local_tile(mB, (self.BN, self.BK), (bidy, None))
+        gC = cute.local_tile(mC, (self.BM, self.BN), (bidx, bidy))
+
+        tAsA, tAgA = cute.nvgpu.tma_partition(
+            tma_atom_a,
+            cute.Int32(0),
+            cute.make_layout((1,)),
+            cute.group_modes(sA, 0, 2),
+            cute.group_modes(gA, 0, 2)
+        )
+
+        tBsB, tBgB = cute.nvgpu.tma_partition(
+            tma_atom_b,
+            cute.Int32(0),
+            cute.make_layout((1,)),
+            cute.group_modes(sB, 0, 2),
+            cute.group_modes(gB, 0, 2)
+        )
+
+        num_tiles_k = cute.size(gA, mode=[2])
+
+        thr_mma = tiled_mma.get_slice(tidx)
+        tCsA = thr_mma.partition_A(gA)
+        tCsB = thr_mma.partition_B(gB)
+        tCgC = thr_mma.partition_C(gC)
+
+        tCrA = thr_mma.make_fragment_A(tCsA);
+        tCrB = thr_mma.make_fragment_B(tCsB);
         
+        tCrC = thr_mma.make_fragment_A(tCgC.shape);
+        tCrC.fill(0.0)
 
-
+        consumer_state = cutlass.utils.make_pipeline_state(cutlass.PipelineUserType.Consumer, self.num_ab_pipeline_stages)
+        producer_state = cutlass.utils.make_pipeline_state(cutlass.PipelineUserType.Producer, self.num_ab_pipeline_stages)
 
 
 
@@ -168,5 +216,12 @@ if __name__ == "__main__":
     A_dlpack = from_dlpack(A, assumed_align=16)
     B_dlpack = from_dlpack(B, assumed_align=16)
     C_dlpack = from_dlpack(C, assumed_align=16)
+    
+    torch_stream = torch.cuda.Stream()
+    stream = cuda.CUstream(torch_stream.cuda_stream)
 
     gemm = HopperGemm()
+    compiled_kernel = cute.compile(gemm, A_dlpack, B_dlpack, C_dlpack, stream)
+    torch_stream.synchronize()
+
+    compiled_kernel(gA, gB, gC, stream)
