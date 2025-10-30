98ae90ab9ba634ecd1126bb65a1dda19a0a77899
sampan26 <rvbh87m@gmail.com>
2025-10-30 00:58:06 +0000

    finish 01

--- cutedsl/blackwell/01_bf16_gemm.py
+++ cutedsl/blackwell/01_bf16_gemm.py
@@ -63,15 +63,141 @@ def kernel(
         barrier_id=1,
         num_threads=threads_per_cta,
     )
-    tmem = cute.utils.TmemAllocator(
+
+    tmem = utils.TmemAllocator(
         storage.tmem_holding_buf,
-        barrier_for_retrieve=tmem_alloc_barrier,
+        barrier_for_retrieve=tmem_alloc_barrier
     )
 
-    # num_tmem_cols = 512
-    # tmem.allocate(num_tmem_cols)
+    num_cols = 512
+    tmem.allocate(num_cols)
 
+    if warp_idx == 0:
+        cpasync.prefetch_descriptor(tma_atom_a)
+        cpasync.prefetch_descriptor(tma_atom_b)
+    
+    num_tma_copy_bytes = cute.size_in_bytes(io_dtype, cute.select(a_smem_layout, mode=[0,1,2])) +cute.size_in_bytes(io_dtype, cute.select(b_smem_layout, mode=[0,1,2]))
 
+    ab_producer, ab_consumer = pipeline.PipelineTmaUmma.create(
+        num_stages=ab_stages,
+        producer_group=pipeline.CooperativeGroup(pipeline.Agent.Thread),
+        consumer_group=pipeline.CooperativeGroup(pipeline.Agent.Thread),
+        tx_count=num_tma_copy_bytes,
+        barrier_storage=storage.ab_mbar_ptr.data_ptr(),
+    ).make_participants()
+    acc_producer, acc_consumer = pipeline.PipelineUmmaAsync.create(
+        num_stages=acc_stage,
+        producer_group=pipeline.CooperativeGroup(pipeline.Agent.Thread),
+        consumer_group=pipeline.CooperativeGroup(
+            pipeline.Agent.Thread,
+            threads_per_cta,
+        ),
+        barrier_storage=storage.acc_mbar_ptr.data_ptr(),
+    ).make_participants()
+
+    gA = cute.local_tile(mA, mma_tiler, coords, proj=(1, None, 1))
+    gB = cute.local_tile(mB, mma_tiler, coords, proj=(None, 1, 1))
+    gC = cute.local_tile(mC, mma_tiler, coords, proj=(1, 1, None))
+
+    thr_mma = tiled_mma.get_slice(0)
+    tCgA = thr_mma.partition_A(gA)
+    tCgB = thr_mma.partition_B(gB)
+    tCgC = thr_mma.partition_C(gC)
+    tCrA = thr_mma.make_fragment_A(sA)
+    tCrB = thr_mma.make_fragment_B(sB)
+
+    acc_shape = tiled_mma.partition_shape_C(mma_tiler[:2])
+    tCtAcc = tiled_mma.make_fragment_C(acc_shape)
+
+    tAsA, tAgA = cpasync.tma_partition(
+        tma_atom_a,
+        0,
+        cute.make_layout(1),
+        cute.group_modes(sA, 0, 3),
+        cute.group_modes(tCgA, 0, 3),
+    ) 
+
+    tBsB, tBgB = cpasync.tma_partition(
+        tma_atom_b,
+        0,
+        cute.make_layout(1),
+        cute.group_modes(sB, 0, 3),
+        cute.group_modes(tCgB, 0, 3),
+    ) 
+
+    tmem.wait_for_alloc()
+    tmem_ptr = tmem.retrieve_ptr(acc_dtype)
+
+    tCtAcc = cute.make_tensor(tmem_ptr, tCtAcc.layout)
+
+    subtile_cnt = 4
+    # (EpiTile)
+    epi_tiler = (
+        (cute.size(tCtAcc, mode=[0, 0]), cute.size(tCtAcc, mode=[0, 1]) // subtile_cnt),
+    )
+
+    tCtAcc_epi = cute.zipped_divide(tCtAcc, epi_tiler)
+    gC_epi = cute.zipped_divide(tCgC, epi_tiler)
+
+    tmem_atom = cute.make_copy_atom(
+        tcgen05.Ld32x32bOp(tcgen05.Repetition.x64),
+        cutlass.Float32
+    )
+    
+    tmem_tiled_copy = tcgen05.make_tmem_copy(tmem_atom, tCtAcc_epi[None, 0])
+    tmem_thr_copy = tmem_tiled_copy.get_slice(tidx)
+
+    tDtC = tmem_thr_copy.partition_S(tCtAcc_epi)
+    tDgC = tmem_thr_copy.partition_D(gC_epi)
+
+    tCrAcc = cute.make_rmem_tensor(tDgC[None, None, 0].shape, acc_dtype)  # Uses tDgC, not tDtC
+    tCrC = cute.make_rmem_tensor(tDgC[None, None, 0].shape, io_dtype)
+
+    num_k_tiles = cute.size(gA, mode=[2])
+    if warp_idx == 0:
+        acc_empty = acc_producer.acquire_and_advance()
+        for k_tile in cutlass.range(num_k_tiles, prefetch_stages=ab_stages - 2):
+            ab_empty = ab_producer.acquire_and_advance()
+            cute.copy(
+                tma_atom_a, 
+                tAgA[(None, ab_empty.count)],
+                tAsA[(None, ab_empty.index)],
+                tma_bar_ptr=ab_empty.barrier,
+            )
+            cute.copy(
+                tma_atom_b, 
+                tBgB[(None, ab_empty.count)],
+                tBsB[(None, ab_empty.index)],
+                tma_bar_ptr=ab_empty.barrier,
+            )
+            
+            ab_full = ab_consumer.wait_and_advance()
+            num_k_blocks = cute.size(tCrA, mode=[2])
+            for block_idx in cutlass.range_constexpr(num_k_blocks):
+                k_block_coords = (None, None, block_idx, ab_full.index)
+                cute.gemm(
+                    tiled_mma,
+                    tCtAcc,
+                    tCrA[k_block_coords],
+                    tCrB[k_block_coords],
+                    tCtAcc,
+                )
+                tiled_mma.set(tcgen05.Field.ACCUMULATE, True)
+            
+            ab_full.release()
+        acc_empty.commit()
+
+    tmem.relinquish_alloc_permit()
+
+    acc_full = acc_consumer.wait_and_advance()
+    for i in cutlass.range(cute.size(tDtC, mode=[2])):
+        cute.copy(tmem_tiled_copy, tDtC[None, None, i], tCrAcc)
+        tCrC.store(tCrAcc.load().to(io_dtype))
+        cute.autovec_copy(tCrC, tDgC[None, None, i])
+    
+    acc_full.release()
+    pipeline.sync(barrier_id=1)    
+    tmem.free(tmem_ptr)
 
 @cute.jit
 def gemm(
@@ -176,7 +302,7 @@ def run_dense_gemm(
     gemm(a_tensor, b_tensor, c_tensor, no_cache=True)
 
     ref = (torch.einsum("mk, nk -> mn", a.to(torch.float32), b.to(torch.float32))).cpu()
-    # torch.testing.assert_close(c.cpu(), ref.to(cutlass_torch.dtype(io_dtype)), atol=tolerance, rtol=1e-05)
+    torch.testing.assert_close(c.cpu(), ref.to(cutlass_torch.dtype(io_dtype)), atol=tolerance, rtol=1e-05)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Blackwell bf16 gemm")
